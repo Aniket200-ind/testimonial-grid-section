d89f03377cd09b8aaa6a1342763874d78db1945a
Aniket <aniketbotre007@gmail.com>
2023-12-04 18:14:25 +0530

    Refactor testimonial container and add CSS styles

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,10 @@
 <script>
   import Container from "./lib/Container.svelte";
   import { testimonials } from "./data";
+  import "./app.css";
 </script>
 <main>
   {#each testimonials as testimonial}
     <Container {testimonial} />
   {/each}
 </main>
-
-<style>
-  
-</style>
--- src/lib/Container.svelte
+++ src/lib/Container.svelte
@@ -1,21 +1,51 @@
 <script>
   export let testimonial;
+  import "../app.css";
+  import "../container.css";
 </script>
 
-<section>
-  <div>
-    <img src={testimonial.imageUrl} alt={testimonial.name} />
-    <p>{testimonial.name}</p>
-    <p>{testimonial.title}</p>
+<section
+  style={`background-color: ${testimonial.bgColor};`}
+  id={testimonial.className}
+>
+  {#if testimonial.id === 1}
+    <img src="images/bg-pattern-quotation.svg" alt="Quotes" id="quote" />
+  {/if}
+  <div class="outer-div">
+    <img src={testimonial.imageUrl} alt={testimonial.name} id="profile-pic" />
+    <div id="inner-div">
+      <p
+        class={testimonial.theme === "dark"
+          ? "light-greyish-blue"
+          : "dark-blackish-blue"}
+        id="name"
+      >
+        {testimonial.name}
+      </p>
+      <p
+        class={testimonial.theme === "dark"
+          ? "light-greyish-blue"
+          : "dark-blackish-blue"}
+        id="title"
+      >
+        {testimonial.title}
+      </p>
+    </div>
   </div>
-  <p>
-    {testimonial.text}
+  <p
+    class={testimonial.theme === "dark"
+      ? "light-greyish-blue"
+      : "dark-blackish-blue"}
+    id="heading"
+  >
+    {testimonial.heading}
   </p>
-  <p>
-    {testimonial.experience}
+  <p
+    class={testimonial.theme === "dark"
+      ? "light-greyish-blue"
+      : "dark-blackish-blue"}
+    id="review"
+  >
+    {testimonial.review}
   </p>
 </section>
-
-<style>
-
-</style>
